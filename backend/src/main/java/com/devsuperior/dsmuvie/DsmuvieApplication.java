17577bf220ff53e7170963b9b45e62f34d53b169
jeftst <jef.m2095@gmail.com>
2022-03-08 02:09:58 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsmuvie/DsmuvieApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmuvie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmuvieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmuvieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsmuvie/DsmuvieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmuvie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmuvieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
