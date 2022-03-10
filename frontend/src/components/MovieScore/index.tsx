bef801bae945c0a7912bdd594a26aba4cc5a82f3
jeftst <jef.m2095@gmail.com>
2022-03-09 23:56:13 -0300

    MovieCard

---
+++ frontend/src/components/MovieScore/index.tsx
@@ -0,0 +1,16 @@
+import MovieStars from "components/MovieStars"
+import './styles.css';
+function MovieScore() {
+
+    const score = 3.5;
+    const count = 13;
+
+    return (
+        <div className="jfmovie-score-container">
+            <p className="jfmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
+            <MovieStars />
+            <p className="jfmovie-score-count">{count} avaliações</p>
+        </div>
+    )
+}
+export default MovieScore;
