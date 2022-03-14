3c9c5f7e565dd1262898bff1d505f63638dedc70
jeftst <jef.m2095@gmail.com>
2022-03-13 21:48:29 -0300

    Pagination

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,15 +1,23 @@
 import {ReactComponent as Arrow} from 'assets/img/arrow.svg';
+import { MoviePage } from 'types/movie';
 import './styles.css';
 
-function Pagination() {
+type Props = {
+    page: MoviePage;
+    onChange: Function;
+}
+
+function Pagination({ page, onChange } : Props) {
+
+
     return (
         <div className="jfmovie-pagination-container">
             <div className="jfmovie-pagination-box">
-                <button className="jfmovie-pagination-button" disabled={true} >
+                <button className="jfmovie-pagination-button" disabled={page.first} onClick = {() => onChange(page.number - 1)} >
                     <Arrow />
                 </button>
-                <p>{`${1} de ${3}`}</p>
-                <button className="jfmovie-pagination-button" disabled={false} >
+                <p>{`${page.number + 1} de ${page.totalPages}`}</p>
+                <button className="jfmovie-pagination-button" disabled={page.last} onClick = {() => onChange(page.number + 1)} >
                     <Arrow className="jfmovie-flip-horizontal" />
                 </button>
             </div>
